a45a5fe2f25bd0f131135b414124097ae68771bf
David Janke <numberMumbler@users.noreply.github.com>
2024-04-04 15:00:52 -0400

    Tweak the fetch script

--- fetch.rb
+++ fetch.rb
@@ -3,17 +3,17 @@
 require 'rss'
 require 'net/http'
 require 'uri'
+require 'logger'
 
-Record = Struct.new(:title, :link, :guid, :authors) do
+CATEGORIES = %w[cs nlin math q-fin stat econ eess].freeze
+
+LOGGER = Logger.new($stdout)
+LOGGER.level = Logger::INFO
+
+PaperInfo = Struct.new(:title, :web_url, :guid, :authors, :pdf_url) do
   def to_s
-    "#{title}\n#{authors}\n#{guid}\n#{link}"
+    "#{title}\n#{authors}\n#{guid}\n#{web_url}\n#{pdf_url}"
   end
-end
-
-def process_feed(url)
-  get_items(url).each { |item| process_item(item) }
-rescue Net::HTTPError, URI::InvalidURIError, RSS::InvalidRSSError => e
-  puts "Failed to fetch or parse the RSS feed: #{e.message}"
 end
 
 def get_items(feed_url)
@@ -29,19 +29,21 @@ def get_items(feed_url)
 end
 
 def process_item(item)
-  record = to_record(item)
-  return if already_processed?(record.guid)
+  paper_info = to_paper_info(item)
+  return if already_processed?(paper_info.guid)
 
-  puts "#{record}\n\n"
+  LOGGER.info(paper_info.to_s)
 
-  mark_as_processed(record.guid)
+  mark_as_processed(paper_info.guid)
 end
 
-def to_record(item)
-  guid = (item.guid.content.match(/^(.+?)v\d+$/)&.captures || []).first
+def to_paper_info(item)
+  id = item.guid.content[/\d+\.\d+/]
   creator = item.dc_creator if item.respond_to?(:dc_creator)
 
-  Record.new(item.title, item.link, guid, creator)
+  pdf_url = "https://export.arxiv.org/pdf/#{id}"
+
+  PaperInfo.new(item.title, item.link, "arXiv:#{id}", creator, pdf_url)
 end
 
 def already_processed?(_unique_id)
@@ -51,7 +53,17 @@ end
 
 def mark_as_processed(unique_id)
   # Logic to record the unique_id in the database or data store as processed
-  # puts "  ** Marking #{unique_id} as processed"
+  LOGGER.info("Marking #{unique_id} as processed")
 end
 
-%w[cs nlin math q-fin stat econ eess].each { |category| process_feed("https://rss.arxiv.org/rss/#{category}?version=2.0") }
+def process_feed(url)
+  get_items(url).each { |item| process_item(item) }
+rescue Net::HTTPError, URI::InvalidURIError, RSS::InvalidRSSError => e
+  LOGGER.error("Failed to fetch or parse the RSS feed: #{e.message}")
+end
+
+def main
+  CATEGORIES.each { |category| process_feed("https://rss.arxiv.org/rss/#{category}?version=2.0") }
+end
+
+main
